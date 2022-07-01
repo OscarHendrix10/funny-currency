b249a04397544026805cd240780e26548f9e8484
OscarHendrix10 <102974462+OscarRodriguez-10@users.noreply.github.com>
2022-07-01 12:02:34 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CurrencyComponent } from './pages/currency/currency.component';
+import { ConvertionComponent } from './pages/convertion/convertion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CurrencyComponent,
+    ConvertionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/convertion/convertion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convertion',
+  templateUrl: './convertion.component.html',
+  styleUrls: ['./convertion.component.css']
+})
+export class ConvertionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
